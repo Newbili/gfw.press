6a3ae4b028cc8a9c56217ae1a673f0bfb01367c7
Newbili <39008175+Newbili@users.noreply.github.com>
2018-07-08 09:51:13 +0800

    Update install_server_centos.sh

--- install_server_centos.sh
+++ install_server_centos.sh
@@ -53,6 +53,7 @@ else
 
         iptables -F ;
         iptables -A INPUT -p tcp --dport 28555 -j ACCEPT ;
+        iptables -A INPUT -p tcp --dport 22 -j ACCEPT ;
         iptables -A INPUT -p tcp --dport 80 -j ACCEPT ;
         iptables -A INPUT -p tcp --dport 443 -j ACCEPT ;
         iptables -A INPUT -p tcp --dport 25 -j ACCEPT ;
